f6b33556c82acf67059f5a2044c69d318b95dafa
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-03-27 11:26:52 +0600

    Implemented signin and signup

---
+++ src/store/actions/auth.js
@@ -0,0 +1,49 @@
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = authData => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData: authData
+    };
+};
+
+export const authFail = error => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const auth = (email, password, isSignUp) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url =
+            "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDf7o_jf02btuBfrOPoGKPG90RKpdDqkcI";
+        if (!isSignUp) {
+            url =
+                "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDf7o_jf02btuBfrOPoGKPG90RKpdDqkcI";
+        }
+        axios
+            .post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
+    };
+};
