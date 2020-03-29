3931026f82efcb73cd210abe09a46d2cb11a0494
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-03-29 11:07:30 +0600

    Converting modal, orderSumamry compo to functional

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,22 +1,21 @@
-import React, { Component } from "react";
-import {Redirect} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React, { useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-import * as actions from '../../../store/actions/index';
+import * as actions from "../../../store/actions/index";
 
-class Logout extends Component {
-    componentDidMount() {
-        this.props.onLogout();
-    }
-    render() {
-        return <Redirect to="/"/>
-    }
-}
+const logout = props => {
+	useEffect(() => {
+		props.onLogout();
+	}, []);
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onLogout: () => dispatch(actions.logout())
-    };
+	return <Redirect to='/' />;
 };
 
-export default connect(null,mapDispatchToProps)(Logout);
+const mapDispatchToProps = dispatch => {
+	return {
+		onLogout: () => dispatch(actions.logout())
+	};
+};
+
+export default connect(null, mapDispatchToProps)(logout);
